8e64486dc5fdb0be3158f6b98403bffe390f2f93
Prateek Bhatnagar <prateek89born@gmail.com>
2018-04-03 15:02:35 -0700

    Update index.js

--- template/src/routes/home/index.js
+++ template/src/routes/home/index.js
@@ -12,7 +12,7 @@ export default class Home extends Component {
 				<Card>
 					<div class={style.cardHeader}>
 						<h2 class=" mdc-typography--title">Home card</h2>
-				            	<div class=" mdc-typography--caption">Welcome to home route</div>
+						<div class=" mdc-typography--caption">Welcome to home route</div>
 					</div>
 					<div class={style.cardBody}>
 						Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
