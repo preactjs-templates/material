c41a3febd8cbc77f17543980ea71cbd36809099f
Simon Appelt <aipt32@gmail.com>
2018-12-18 11:27:54 +0100

    Fixes ESLint warnings

--- template/src/components/app.js
+++ template/src/components/app.js
@@ -22,12 +22,12 @@ export default class App extends Component {
 	render() {
 		return (
 			<div id="app">
-				<Header selectedRoute={this.state.currentUrl}/>
+				<Header selectedRoute={this.state.currentUrl} />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
-          <NotFound default />
+					<NotFound default />
 				</Router>
 			</div>
 		);
